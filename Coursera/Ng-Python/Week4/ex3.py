a3dd9cbafa5bdef6d744317ed87aeae30b0b4a10
lalit527 <lalit.slg007@gmail.com>
2018-08-10 20:12:14 +0530

    Added problems in python

--- Coursera/Ng-Python/Week4/ex3.py
+++ Coursera/Ng-Python/Week4/ex3.py
@@ -102,24 +102,40 @@ def predictOneVsAll(all_theta, features):
 pred = predictOneVsAll(theta, X)
 print('Accuracy: {} %'.format(np.mean(pred == y.ravel())*100))
 
-###### Below ############
-def oneVsAll(features, classes, n_labels, reg):
-    initial_theta = np.zeros((X.shape[1],1))  # 401x1
-    all_theta = np.zeros((n_labels, X.shape[1])) #10x401
+# Multiclass Logistic Regression
+clf = LogisticRegression(C=10, penalty='l2', solver='liblinear')
+clf.fit(X[:,1:], y.ravel())
 
-    for c in np.arange(1, n_labels+1):
-        res = minimize(lrcostFunctionReg, initial_theta, args=(reg, features, (classes == c)*1), method=None,
-                       jac=lrgradientReg, options={'maxiter':50})
-        all_theta[c-1] = res.x
-    return(all_theta)
+pred2 = clf.predict(X[:,1:])
+print('Training set accuracy: {} %'.format(np.mean(pred2 == y.ravel())*100))
+
+def predict(theta_1, theta_2, features):
+    z2 = theta_1.dot(features.T)
+    a2 = np.c[np.ones((data['X'].shape[0],1)), sigmoid(z2).T]
     
-theta = oneVsAll(X, y, 10, 0.1)
+    z3 = a2.dot(theta_2.T)
+    a3 = sigmoid(z3)
+    
+    return (np.argmax(a3, axis=1) + 1)
 
-def predictOneVsAll(all_theta, features):
-    probs = sigmoid(X.dot(all_theta.T))
+pred = predict(theta1, theta2, X)
+
+###### Below ############
+clf = LogisticRegression(C=10, penalty='l2', solver='liblinear')
+# Scikit-learn fits intercept automatically, so we exclude first column with 'ones' from X when fitting.
+clf.fit(X[:,1:],y.ravel())
+
+pred2 = clf.predict(X[:,1:])
+print('Training set accuracy: {} %'.format(np.mean(pred2 == y.ravel())*100))
+
+def predict(theta_1, theta_2, features):
+    z2 = theta_1.dot(features.T)
+    a2 = np.c_[np.ones((data['X'].shape[0],1)), sigmoid(z2).T]
+    
+    z3 = a2.dot(theta_2.T)
+    a3 = sigmoid(z3)
         
-    # Adding one because Python uses zero based indexing for the 10 columns (0-9),
-    # while the 10 classes are numbered from 1 to 10.
-    return(np.argmax(probs, axis=1)+1)
-pred = predictOneVsAll(theta, X)
+    return(np.argmax(a3, axis=1)+1)
+    
+pred = predict(theta1, theta2, X)
 print('Training set accuracy: {} %'.format(np.mean(pred == y.ravel())*100))
