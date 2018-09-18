5cda0019c369d1a3e1abc298ce7641311d4584a1
lalit527 <lalit.slg007@gmail.com>
2018-09-18 07:09:05 +0530

    Added SVM classifier algorithm

--- Udemy/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
+++ Udemy/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
@@ -1,4 +1,12 @@
-# Support Vector Machine (SVM)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 18 07:03:54 2018
+
+@author: lality
+
+algorithm: Support Vector Machine (SVM)
+"""
 
 # Importing the libraries
 import numpy as np
@@ -20,9 +28,10 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting SVM to the Training set
+# Fitting classifier to the Training set
+# SVM Classifier
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
+classifier = SVC(kernel= 'linear', random_state=0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -44,7 +53,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Training set)')
+plt.title('SVM Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -62,7 +71,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Test set)')
+plt.title('SVM Classifier (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
