94a3020f54d1ac17c9938909919d053ecb052416
lalit527 <lalit.slg007@gmail.com>
2018-10-02 10:01:42 +0530

    Added UCB algo for reinforcement

--- Udemy/Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/UCB/upper_confidence_bound.py
+++ Udemy/Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/UCB/upper_confidence_bound.py
@@ -1,4 +1,12 @@
-# Upper Confidence Bound
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  2 09:27:09 2018
+
+@author: lality
+
+algorithm: Upper Confidence bound
+"""
 
 # Importing the libraries
 import numpy as np
@@ -8,21 +16,23 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
-# Implementing UCB
-import math
-N = 10000
-d = 10
+X = np.array(dataset)
+
+# Implementing the UCB
+import random
+N = X.shape[0]
+d = X.shape[1]
 ads_selected = []
-numbers_of_selections = [0] * d
+number_of_selections = [0] * d
 sums_of_rewards = [0] * d
 total_reward = 0
-for n in range(0, N):
+for n in range(N):
     ad = 0
     max_upper_bound = 0
-    for i in range(0, d):
-        if (numbers_of_selections[i] > 0):
-            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
-            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+    for i in range(d):
+        if number_of_selections[i] > 0:
+            average_reward = sums_of_rewards[i] / number_of_selections[i]
+            delta_i = math.sqrt(3/2 * math.log(n + 1) / number_of_selections[i])
             upper_bound = average_reward + delta_i
         else:
             upper_bound = 1e400
@@ -30,11 +40,11 @@ for n in range(0, N):
             max_upper_bound = upper_bound
             ad = i
     ads_selected.append(ad)
-    numbers_of_selections[ad] = numbers_of_selections[ad] + 1
+    number_of_selections[ad] += 1
     reward = dataset.values[n, ad]
-    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
-    total_reward = total_reward + reward
-
+    sums_of_rewards[ad] += reward
+    total_reward += reward
+    
 # Visualising the results
 plt.hist(ads_selected)
 plt.title('Histogram of ads selections')
