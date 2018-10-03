9fb1afac65e0e8068ce06f083c11c93da1630666
lalit527 <lalit.slg007@gmail.com>
2018-10-03 08:33:33 +0530

    NLP Algorithm

--- Udemy/Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/Natural_Language_Processing/natural_language_processing.py
+++ Udemy/Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/Natural_Language_Processing/natural_language_processing.py
@@ -1,4 +1,12 @@
-# Natural Language Processing
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 07:58:32 2018
+
+@author: lality
+
+algorithm: Natural Language Processing
+"""
 
 # Importing the libraries
 import numpy as np
@@ -15,7 +23,7 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 corpus = []
-for i in range(0, 1000):
+for i in range(dataset.shape[0]):
     review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
     review = review.lower()
     review = review.split()
@@ -23,10 +31,10 @@ for i in range(0, 1000):
     review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)
     corpus.append(review)
-
-# Creating the Bag of Words model
+    
+# Creating the bag of words model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
+cv = CountVectorizer(max_features= 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
@@ -34,14 +42,34 @@ y = dataset.iloc[:, 1].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# Fitting Naive Bayes to the Training set
+# Fitting Navie Bayes to the Training set
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
+# Predicting the Test set result
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+"""
+TP = # True Positives, TN = # True Negatives, FP = # False Positives, FN = # False Negatives
+
+Accuracy = (TP + TN) / (TP + TN + FP + FN)
+
+Precision = TP / (TP + FP)
+
+Recall = TP / (TP + FN)
+
+F1 Score = 2 * Precision * Recall / (Precision + Recall)
+"""
+
+accuracy = (cm[0][0] + cm[1][1]) / (cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1])
+precision = (cm[0][0]) / (cm[0][0] + cm[1][0])
+recall = (cm[0][0]) / (cm[0][0] + cm[0][1])
+f1 = (2 * precision * recall) / (precision + recall)
+
+
+
